27dffec090769b51dfee92bf3349b81de1a045d9
Javid Ahmed <javidahmed@icloud.com>
2024-03-25 00:53:42 +0000

    Add docstring and update descriptions

--- src/main.py
+++ src/main.py
@@ -5,7 +5,13 @@ from helpers import system_msg
 from steganographer import Steganographer
 
 
-def main(args):
+def main(args: argparse.Namespace) -> None:
+    """
+    Run steganography application with args.
+
+    Parameters:
+        args (Namespace): Console arguments, see below
+    """
     steg = Steganographer()
     if args.encode:
         steg.encode_img(Path(args.input_img[0]), args.msg[0], args.output_img[0])
@@ -20,8 +26,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Steganography application.")
     parser.add_argument("-encode", action="store_true", help="Insert a message into an image")
     parser.add_argument("-decode", action="store_true", help="Extract a message from an image")
-    parser.add_argument("--input_img", type=str, nargs="+", help="Path to image file")
-    parser.add_argument("--msg", type=str, nargs="+", help="Message to insert into image")
-    parser.add_argument("--output_img", type=str, nargs="+", help="Name for encoded image")
+    parser.add_argument("--input_img", type=str, nargs="+", help="Path to image file to encode/decode")
+    parser.add_argument("--msg", type=str, nargs="+", help="Message to insert into image if encoding")
+    parser.add_argument("--output_img", type=str, nargs="+", help="Name for encoded image if encoding")
     args = parser.parse_args()
     main(args)
